d4091390a1d5cd38899ee82f221b0e9613322f16
petaccia <100707989+petaccia@users.noreply.github.com>
2025-07-02 16:37:50 +0200

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: true, // point virgule obligatoire
+  singleQuote: false, // guillemets doubles
+  trailingComma: "all", // virgule à la fin de l'objet
+  arrowParens: "avoid", // parenthèses obligatoires
+  printWidth: 80, // largeur de la ligne
+  tabWidth: 2, // largeur de l'indentation
+  useTabs: false, // utilisation des tabulations
+  bracketSpacing: true, // espace entre les parenthèses
+  bracketSameLine: false, // même ligne pour les parenthèses
+  endOfLine: "lf", // fin de ligne
+};
